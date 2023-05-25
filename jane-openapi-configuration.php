e18a5c4e4afe0fbc5adb4e603b351c2308e21992
Wolfgang <wolfgang.herka@datenkraft.com>
2023-05-25 09:23:49 +0200

    init names

--- jane-openapi-configuration.php
+++ jane-openapi-configuration.php
@@ -2,7 +2,7 @@
 
 return [
     'openapi-file' => __DIR__ . '/openapi.json',
-    'namespace' => 'Datenkraft\Backbone\Client\XxxApi\Generated',
+    'namespace' => 'Datenkraft\Backbone\Client\OrganizationStructureApi\Generated',
     'directory' => __DIR__ . '/src/Generated',
     'throw-unexpected-status-code' => true,
 ];
