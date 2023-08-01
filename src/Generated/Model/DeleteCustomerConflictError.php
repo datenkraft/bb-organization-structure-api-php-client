ae05d4aebecd29ea93a439207444ad5fb4659552
Datenkraft <office@datenkraft.com>
2023-08-01 15:45:36 +0000

    Autogenerated client update [publish]

--- src/Generated/Model/DeleteCustomerConflictError.php
+++ src/Generated/Model/DeleteCustomerConflictError.php
@@ -25,6 +25,12 @@ class DeleteCustomerConflictError extends \ArrayObject
      */
     protected $message;
     /**
+     * References
+     *
+     * @var ErrorReferencesItem[]
+     */
+    protected $references;
+    /**
      * Extra
      *
      * @var DeleteCustomerConflictErrorextra
@@ -72,6 +78,28 @@ class DeleteCustomerConflictError extends \ArrayObject
     {
         $this->initialized['message'] = true;
         $this->message = $message;
+        return $this;
+    }
+    /**
+     * References
+     *
+     * @return ErrorReferencesItem[]
+     */
+    public function getReferences() : array
+    {
+        return $this->references;
+    }
+    /**
+     * References
+     *
+     * @param ErrorReferencesItem[] $references
+     *
+     * @return self
+     */
+    public function setReferences(array $references) : self
+    {
+        $this->initialized['references'] = true;
+        $this->references = $references;
         return $this;
     }
     /**
