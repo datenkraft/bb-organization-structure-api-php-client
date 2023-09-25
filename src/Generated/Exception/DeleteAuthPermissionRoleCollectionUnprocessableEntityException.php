c3007f9f7f1ea55168c1c1cd0d92455f23bbc397
Datenkraft <office@datenkraft.com>
2023-09-25 09:07:49 +0000

    Autogenerated client update [publish]

--- src/Generated/Exception/DeleteAuthPermissionRoleCollectionUnprocessableEntityException.php
+++ src/Generated/Exception/DeleteAuthPermissionRoleCollectionUnprocessableEntityException.php
@@ -2,7 +2,7 @@
 
 namespace Datenkraft\Backbone\Client\OrganizationStructureApi\Generated\Exception;
 
-class DeleteAuthPermissionRoleCollectionNotFoundException extends NotFoundException
+class DeleteAuthPermissionRoleCollectionUnprocessableEntityException extends UnprocessableEntityException
 {
     /**
      * @var \Datenkraft\Backbone\Client\OrganizationStructureApi\Generated\Model\ErrorResponse
@@ -14,7 +14,7 @@ class DeleteAuthPermissionRoleCollectionNotFoundException extends NotFoundExcept
     private $response;
     public function __construct(\Datenkraft\Backbone\Client\OrganizationStructureApi\Generated\Model\ErrorResponse $errorResponse, \Psr\Http\Message\ResponseInterface $response)
     {
-        parent::__construct('Not Found');
+        parent::__construct('Unprocessable Entity');
         $this->errorResponse = $errorResponse;
         $this->response = $response;
     }
